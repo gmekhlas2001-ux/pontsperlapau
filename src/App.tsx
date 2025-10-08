f251b77e46400e67bac6016790f978a8156c3d91
murtaza ekhlas <gm.ekhlas2001@gmail.com>
2025-10-08 16:23:03 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ import { Profile } from './pages/Profile';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/pontsperlapau">
       <AuthProvider>
         <Routes>
           <Route path="/login" element={<Login />} />
