0da6b8d72c19e14c836a8fabf9dca302346abcd3
murtaza ekhlas <gm.ekhlas2001@gmail.com>
2025-10-09 21:50:26 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // ✅ Netlify expects root
+  base: '/pontsperlapau/', // ✅ GitHub Pages expects subpath
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
