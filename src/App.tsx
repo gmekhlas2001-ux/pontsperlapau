2d0c464865b7bdf92d10b26ea09f5670837540c6
murtaza ekhlas <gm.ekhlas2001@gmail.com>
2025-10-09 15:17:45 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { ProtectedRoute } from './components/ProtectedRoute';
 import { Layout } from './components/Layout';
@@ -17,116 +17,114 @@ import { Profile } from './pages/Profile';
 
 function App() {
   return (
-    <BrowserRouter basename="/pontsperlapau">
-      <AuthProvider>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/pending-approval" element={<PendingApproval />} />
+    <AuthProvider>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/pending-approval" element={<PendingApproval />} />
 
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <Dashboard />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Layout>
+                <Dashboard />
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
 
-          <Route
-            path="/approvals"
-            element={
-              <ProtectedRoute requireAdmin>
-                <Layout>
-                  <Approvals />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
+        <Route
+          path="/approvals"
+          element={
+            <ProtectedRoute requireAdmin>
+              <Layout>
+                <Approvals />
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
 
-          <Route
-            path="/staff"
-            element={
-              <ProtectedRoute requireAdmin>
-                <Layout>
-                  <Staff />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
+        <Route
+          path="/staff"
+          element={
+            <ProtectedRoute requireAdmin>
+              <Layout>
+                <Staff />
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
 
-          <Route
-            path="/students"
-            element={
-              <ProtectedRoute requireAdmin>
-                <Layout>
-                  <Students />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
+        <Route
+          path="/students"
+          element={
+            <ProtectedRoute requireAdmin>
+              <Layout>
+                <Students />
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
 
-          <Route
-            path="/branches"
-            element={
-              <ProtectedRoute requireAdmin>
-                <Layout>
-                  <Branches />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
+        <Route
+          path="/branches"
+          element={
+            <ProtectedRoute requireAdmin>
+              <Layout>
+                <Branches />
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
 
-          <Route
-            path="/classrooms"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <Classrooms />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
+        <Route
+          path="/classrooms"
+          element={
+            <ProtectedRoute>
+              <Layout>
+                <Classrooms />
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
 
-          <Route
-            path="/libraries/books"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <Library />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
+        <Route
+          path="/libraries/books"
+          element={
+            <ProtectedRoute>
+              <Layout>
+                <Library />
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
 
-          <Route
-            path="/reports"
-            element={
-              <ProtectedRoute requireAdmin>
-                <Layout>
-                  <Reports />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
+        <Route
+          path="/reports"
+          element={
+            <ProtectedRoute requireAdmin>
+              <Layout>
+                <Reports />
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
 
-          <Route
-            path="/profile"
-            element={
-              <ProtectedRoute>
-                <Layout>
-                  <Profile />
-                </Layout>
-              </ProtectedRoute>
-            }
-          />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Layout>
+                <Profile />
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
 
-          <Route path="/" element={<Navigate to="/dashboard" replace />} />
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+      </Routes>
+    </AuthProvider>
   );
 }
 
