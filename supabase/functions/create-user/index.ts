21cbb734ee5dc3a2eb0d861ff8f00b4767fe77e0
murtaza ekhlas <gm.ekhlas2001@gmail.com>
2026-03-03 17:45:15 +0100

    Updated index.ts

--- supabase/functions/create-user/index.ts
+++ supabase/functions/create-user/index.ts
@@ -5,7 +5,7 @@ import { hash } from "npm:bcryptjs@2.4.3";
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "POST, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Client-Info, Apikey",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Client-Info, Apikey, X-API-Key",
 };
 
 interface CreateUserRequest {
